58e05a8dc74987c0958cfa58de2373f1d94b1384
Markus Schlueter <mschlueter@geomar.de>
2020-03-25 23:56:59 +0100

    submodule SGCT update

---
+++ gitSubmoduleCheatSheet.sh
@@ -0,0 +1,19 @@
+
+# clone and checkout everything to specific branch:
+
+git clone --recursive https://github.com/tychonaut/OpenSpace.git
+
+cd OpenSpace
+
+git checkout feature/domemasterWarp
+
+git submodule update --init --recursive --rebase --force
+
+
+# build :
+
+
+# commit changes in all modules:
+
+# push commits in all modules to their respective remote repos:
+
